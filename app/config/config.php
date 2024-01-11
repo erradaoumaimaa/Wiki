4a8434f8b92a08d4757656b81f5f80b6b0904be9
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-01-11 21:35:55 +0100

    commit

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'wiki');
+define('DB_NAME', 'db_wiki');
 
 // App Root  
 define('APPROOT', dirname(dirname(__FILE__)));
--- app/views/include/header.php
+++ app/views/include/header.php
@@ -5,4 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
+
+<title><?php echo SITENAME; ?></title>
 </head>

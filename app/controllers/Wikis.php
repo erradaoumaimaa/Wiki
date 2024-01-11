afc1320fc3883b9e6f65dbee5aca835b0c9d0571
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-01-11 14:39:13 +0100

    commit

--- app/controllers/Wikis.php
+++ app/controllers/Wikis.php
@@ -1,33 +1,38 @@
 <?php
+class Wikis extends Controller
+{
+    private $userModel;
+    private $wikiModel;
+    private $categoryModel;
+    private $tagModel;
 
-
-class Wikis extends Controller {
-
-    public function create() {
-
-        // form to create a new wiki :
+    public function __construct()
+    {
+        $this->userModel = $this->model('User');
+        $this->wikiModel = $this->model('Wiki');
+        $this->categoryModel = $this->model('Category');
+        $this->tagModel = $this->model('Tag');
     }
-
-    public function store() {
-
-        // form submission and store the new wiki
+    public function addWiki(){
+        $data = [
+            'categories'=> $this->categoryModel->getCats(),
+            'tags'=> $this->tagModel->getTags(),
+        ];
+        $this->view('wikis/addWiki',$data);
     }
+    public function insertData(){
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $data=[
+            'title'=> trim($_POST['title']),
+            'content'=> trim($_POST['content']),
+            'category_id'=> trim($_POST['category']),
+            'tags'=> isset($_POST['tag']) ? $_POST['tag'] : [],
+        ];
 
-    public function edit($id) {
 
-        // form to edit the specified wiki
+        $lastId = $this->wikiModel->Add($data);
+        foreach ($data['tags'] as $tag) {
+            $this->wikiModel->attachTag($lastId,$tag);
+        }
     }
-
-    public function update($id) {
-
-        // form submission and update the wiki
-    }
-
-    public function delete($id) {
-        
-        // Delete the specified wiki
-    }
-    
-
-
 }
